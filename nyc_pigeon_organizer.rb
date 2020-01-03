fcb41bc8c9e03a3dbedb56f499697bd43bb17339
Eddie Hernandez <mchh.004@gmail.com>
2020-01-03 18:29:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,14 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each { |attributes, hash| hash.each{ |pigeons, array| array.each { |name| new_hash = {:color => [], :gender => [], :lives => []}}}
-  data[:color].each { |atty, name_array| name_array.each { |name| if data[:color][atty].include?(name)
-    new_hash[name][:color] << atty.to_s
+  data.each { |attributes, hash|
+    hash.each { |pigeons, array|
+      array.each { |name|
+        new_hash = {:color => [], :gender => [], :lives => []}}}
+
+  data[:color].each { |pigeon, array|
+    array.each { |pigeon|
+      if data[:color][atty].include?(name)
+        new_hash[name][:color] << atty.to_s
+
   # write your code here!
 end

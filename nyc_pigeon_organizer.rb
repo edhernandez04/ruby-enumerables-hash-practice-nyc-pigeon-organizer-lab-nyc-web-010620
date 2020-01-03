dae1bf6a8214098bca01ad7c3cc8987fb165c76c
Eddie Hernandez <mchh.004@gmail.com>
2020-01-03 19:00:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,21 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each do |tier1, hash|
-    hash.each do | tier2, array|
-      array.each do | name |
-        if new_hash[name].has_key?(name)
-        new_hash[name] = {}
-      end
-        if new_hash[name].has_key?(tier1)
-          new_hash[name][tier1] = []
-      end
-        if new_hash[name][tier1].include?(tier2)
-          new_hash[name][tier1] << tier2.to_s
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name].has_key?(property)
+          new_hash[name][property] = []
+        end
+
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property] << attribute.to_s
+        end
       end
     end
   end
-end
   new_hash
-  # write your code here!
 end

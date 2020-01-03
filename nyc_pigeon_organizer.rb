64d796d4c2617b43bb9500d4206468a6c594e52f
Eddie Hernandez <mchh.004@gmail.com>
2020-01-03 18:54:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,13 +4,13 @@ def nyc_pigeon_organizer(data)
     hash.each { | tier2, array|
       array.each { | name | if new_hash[name].has_key?(name)
         new_hash[name] = {}
-        }
+        }}}
         if new_hash[name].has_key?(tier1)
           new_hash[name][tier1] = []
-        }
+
         if new_hash[name][tier1].include?(tier2)
           new_hash[name][tier1] << tier2.to_s
-      }
+      end
   new_hash
   # write your code here!
 end

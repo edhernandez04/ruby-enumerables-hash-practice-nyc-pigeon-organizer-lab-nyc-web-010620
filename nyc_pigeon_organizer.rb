0de4b78e492572d30c54affc64d84237038967cd
Eddie Hernandez <mchh.004@gmail.com>
2020-01-03 18:58:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,19 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each { |tier1, hash|
-    hash.each { | tier2, array|
-      array.each { | name | if new_hash[name].has_key?(name)
+  data.each do |tier1, hash|
+    hash.each do | tier2, array|
+      array.each do | name |
+        if new_hash[name].has_key?(name)
         new_hash[name] = {}
-}
+      end
         if new_hash[name].has_key?(tier1)
           new_hash[name][tier1] = []
-end
-}
+      end
         if new_hash[name][tier1].include?(tier2)
           new_hash[name][tier1] << tier2.to_s
-end
-}
+      end
+    end
+  end
 end
   new_hash
   # write your code here!
